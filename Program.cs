469b2cf0c1e157eea811b299320749377e1fe0f0
jsakamoto <junichi.jp@gmail.com>
2023-09-20 21:52:38 +0900

    Yet another fallback implementation

--- Program.cs
+++ Program.cs
@@ -10,16 +10,16 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 ConfigureServices(builder.Services, builder.HostEnvironment.BaseAddress);
 
+// Register the BlazorJS.WebWorkers services only when this app is running in a Web browser.
+// (The BlazorWasmPreRendering.Build will invoke only the ConfigureServices method to pre-render inside a build process.)
+builder.Services.AddBlazorJSRuntime();
+builder.Services.AddWebWorkerService();
+builder.Services.AddSingleton<IMyWorker, MyWorker>();
+
 // BlazorJS.WebWorkers
 await builder.Build().BlazorJSRunAsync();
 
 static void ConfigureServices(IServiceCollection services, string baseAddress)
 {
     services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(baseAddress) });
-
-    // BlazorJS.WebWorkers
-    services.AddBlazorJSRuntime();
-    services.AddWebWorkerService();
-
-    services.AddSingleton<IMyWorker, MyWorker>();
 }
